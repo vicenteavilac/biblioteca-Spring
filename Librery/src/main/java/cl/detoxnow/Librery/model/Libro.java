b044b9002f4d16e3fa4206b0e57e88b55818c457
Plaza Vespucio <plazavespucio@PVAWSB03LC0124.duoc.acad>
2025-05-03 11:12:42 -0400

    Actualiza modelo Libro y conexión a Neon

--- Librery/src/main/java/cl/detoxnow/Librery/model/Libro.java
+++ Librery/src/main/java/cl/detoxnow/Librery/model/Libro.java
@@ -4,16 +4,37 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Libro")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Libro {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
-    private String isbn;
-    private String titulo;
-    private String editorial;
-    private int fechaPublicacion;
-    private String autor;
 
+    @Column(name = "isbn")
+    private String isbn;
+
+    @Column(name = "titulo")
+    private String titulo;
+
+    @Column(name = "editorial")
+    private String editorial;
+
+    @Column(name = "fecha_publicacion")
+    private int fechaPublicacion;
+
+    @Column(name = "autor")
+    private String autor;
 }
